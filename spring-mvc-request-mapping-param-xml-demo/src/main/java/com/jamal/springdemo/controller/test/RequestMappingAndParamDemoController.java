48b0d62bfa3597596c51cb6dadb9b2687180b241
jamalgithub <jelmoubarik@gmail.com>
2017-11-03 15:09:16 +0000

    03/11/2017 at 15:09

---
+++ spring-mvc-request-mapping-param-xml-demo/src/main/java/com/jamal/springdemo/controller/test/RequestMappingAndParamDemoController.java
@@ -0,0 +1,202 @@
+package com.jamal.springdemo.controller.test;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping(value = "/requestMappingAndParamDemo")
+@Controller
+public class RequestMappingAndParamDemoController {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(RequestMappingAndParamDemoController.class);
+
+	/** 
+	 * http://localhost:8080/spring-mvc-request-mapping-param-xml-demo<b>/requestMappingAndParamDemo/home</b>
+	 * @return View name
+	 */
+	@RequestMapping(value = "/home")
+	public String home() {
+		return "requestMappingAndParamHome";
+	}
+
+	/**
+	 * test 1: Testing <b>@RequestParam</b> without <b>explicit attributes</b><br>
+	 * http://localhost:8080/spring-mvc-request-mapping-param-xml-demo/requestMappingAndParamDemo/test1?<b>orgname=xxx</b>
+	 * @param orgName
+	 * @param model
+	 * @return View name
+	 */
+	@RequestMapping(value = "/test1")
+	public String requestMappingAndParamTest1(@RequestParam("orgname") String orgName, Model model) {
+		model.addAttribute("orgname", orgName);
+		model.addAttribute("testserial", "test1");
+		
+		return "requestMappingAndParamResults";
+	}
+
+	/**
+	 * test 2: Testing <b>@RequestMapping</b> with <b>method</b> attribute<br>
+	 * http://localhost:8080/spring-mvc-request-mapping-param-xml-demo/requestMappingAndParamDemo/test2?orgname=xxx
+	 * @param orgName
+	 * @param model
+	 * @return View name
+	 */
+	@RequestMapping(value = "/test2", method = RequestMethod.GET)
+	public String requestMappingAndParamTest2(@RequestParam("orgname") String orgName, Model model) {
+		model.addAttribute("orgname", orgName);
+		model.addAttribute("testserial", "test2");
+		
+		return "requestMappingAndParamResults";
+	}
+
+	/**
+	 * test 3: Testing <b>@RequestMapping</b> with <b>fall back</b> feature<br>
+	 * http://localhost:8080/spring-mvc-request-mapping-param-xml-demo/requestMappingAndParamDemo/<b>[ , xxx, test3, test3/xxx]</b>
+	 * @return View name
+	 */
+	@RequestMapping(value = "*", method = RequestMethod.GET)
+	// @RequestMapping(value="*", method = {RequestMethod.GET, RequestMethod.POST})
+	public String requestMappingAndParamTest3() {
+		return "fallback";
+	}
+
+	/**
+	 * test 4: Testing <b>@RequestParam</b> with <b>defaultValue</b> attribute<br>
+	 * http://localhost:8080/spring-mvc-request-mapping-param-xml-demo/requestMappingAndParamDemo/test4<b>[</b>?orgname=smi<b>]</b>
+	 * @param orgName
+	 * @param model
+	 * @return View name
+	 */
+	@RequestMapping(value = "/test4")
+	public String requestMappingAndParamTest4(@RequestParam(value = "orgname", defaultValue = "Anonymous Organization") String orgName, Model model) {
+		model.addAttribute("orgname", orgName);
+		model.addAttribute("testserial", "test4");
+		
+		return "requestMappingAndParamResults";
+	}
+
+	/**
+	 * test 5: Testing <b>@RequestParam</b> without <b>'name'</b>or <b>'value'</b> attributes<br>
+	 * request param and handler method parameter name must be the same<br>
+	 * http://localhost:8080/spring-mvc-request-mapping-param-xml-demo/requestMappingAndParamDemo/test5?<b>orgname</b>=smi
+	 * @param orgname
+	 * @param model
+	 * @return View name
+	 */
+	@RequestMapping(value = "/test5", method = RequestMethod.GET)
+	public String requestMappingAndParamTest5(@RequestParam String orgname, Model model) {
+		model.addAttribute("orgname", orgname);
+		model.addAttribute("testserial", "test5");
+		
+		return "requestMappingAndParamResults";
+	}
+
+	/**
+	 * test 6, subtest 1: Testing removal of <b>@RequestMapping</b> ambiguity with the same base <b>URI</b> but with a <b>different parameter</b><br>
+	 * http://localhost:8080/spring-mvc-request-mapping-param-xml-demo/requestMappingAndParamDemo<b>/test6</b>?<b>orgname</b>=<b>xxx</b>
+	 * @param orgname
+	 * @param model
+	 * @return View name
+	 */
+	@RequestMapping(value = "/test6", params = "orgname")
+	public String requestMappingAndParamTest6Subtest1(@RequestParam String orgname, Model model) {
+		model.addAttribute("orgname", orgname);
+		model.addAttribute("testserial", "test6-subtest1");
+		
+		return "requestMappingAndParamResults2";
+	}
+
+	/**
+	 * test 6, subtest 2: Testing removal of @RequestMapping ambiguity with <b>the same base URI</b> but with a <b>different parameter</b><br>
+	 * http://localhost:8080/spring-mvc-request-mapping-param-xml-demo/requestMappingAndParamDemo<b>/test6</b>?<b>empcount</b>=<b>xxx</b>
+	 * @param empcount
+	 * @param model
+	 * @return View name
+	 */
+	@RequestMapping(value = "/test6", params = "empcount")
+	public String requestMappingAndParamTest6Subtest2(@RequestParam String empcount, Model model) {
+		model.addAttribute("orgname", empcount);
+		model.addAttribute("testserial", "test6-subtest2");
+		
+		return "requestMappingAndParamResults2";
+	}
+	
+	/**
+	 * test 6, subtest 3: Testing removal of @RequestMapping with <b>multiple request params</b><br>
+	 * http://localhost:8080/spring-mvc-request-mapping-param-xml-demo/requestMappingAndParamDemo/test6/subtest3?<b>orgname</b>=xxxx&<b>empcount</b>=yyy
+	 * @param orgname
+	 * @param empcount
+	 * @param model
+	 * @return View name
+	 */
+	@RequestMapping(value = "/test6/subtest3", method = RequestMethod.GET, params = { "orgname", "empcount" })
+	public String requestMappingAndParamTest6Subtest3(@RequestParam String orgname, @RequestParam String empcount, Model model) {
+		model.addAttribute("orgname", orgname);
+		model.addAttribute("empCount", empcount);
+		model.addAttribute("testserial", "test6-subtest3");
+		
+		return "requestMappingAndParamResults2";
+	}
+	
+	/**
+	 * test 6, subtest 4: Testing <b>@RequestMapping</b> with <b>multiple request params</b> and <b>@RequestParam</b> with <b>single param</b><br>
+	 * http://localhost:8080/spring-mvc-request-mapping-param-xml-demo/requestMappingAndParamDemo/test6/subtest4?<b>orgname</b>=xxxx&<b>empcount</b>=yyy
+	 * @param orgname
+	 * @param model
+	 * @return View name
+	 */
+	@RequestMapping(value = "/test6/subtest4", method = RequestMethod.GET, params = { "orgname", "empcount" })
+	public String requestMappingAndParamTest6Subtest4(@RequestParam String orgname, Model model) {
+		model.addAttribute("orgname", orgname);
+		model.addAttribute("testserial", "test6-subtest4");
+		
+		return "requestMappingAndParamResults2";
+	}
+	
+	/**
+	 * test 7 & 8: Testing <b>@RequestParam</b> with <b>multiple request URI's</b><br>
+	 * http://localhost:8080/spring-mvc-request-mapping-param-xml-demo/requestMappingAndParamDemo<b>/test7</b>?orgname=xxxx<br>
+	 * http://localhost:8080/spring-mvc-request-mapping-param-xml-demo/requestMappingAndParamDemo<b>/test8</b>?orgname=xxxx
+	 * @param orgname
+	 * @param model
+	 * @param request
+	 * @return View name
+	 */
+	@RequestMapping(value = {"/test7", "/test8"}, method = RequestMethod.GET)
+	public String requestMappingAndParamTest7and8(@RequestParam String orgname, Model model, HttpServletRequest request) {
+		model.addAttribute("orgname", orgname);
+		
+		LOGGER.info(request.getRequestURL().toString());
+		
+		if(request.getRequestURL().toString().contains("test7")) {
+			model.addAttribute("testserial", "test7");
+		} else {
+			model.addAttribute("testserial", "test8");
+		}
+		
+		return "requestMappingAndParamResults2";
+	}
+	
+	/**
+	 * test 9: Testing <b>@RequestMapping</b> with <b>Dynamic URI's</b><br>
+	 * http://localhost:8080/spring-mvc-request-mapping-param-xml-demo/requestMappingAndParamDemo<b>/test9</b>/<b>xxx</b>/<b>yyy</b>
+	 * @param orgname
+	 * @param model
+	 * @return View name
+	 */
+	@RequestMapping(value = "/test9/{orgname}/{testserial}", method = RequestMethod.GET)
+	public String requestMappingAndParamTest9(@PathVariable String orgname, @PathVariable String testserial, Model model) {
+		model.addAttribute("orgname", orgname);
+		model.addAttribute("testserial", testserial);
+		
+		return "requestMappingAndParamResults";
+	}
+
+}
---
+++ spring-mvc-request-mapping-param-xml-demo/src/main/webapp/WEB-INF/views/requestMappingAndParamHome.jsp
@@ -0,0 +1,76 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+	<%@ taglib uri = "http://java.sun.com/jsp/jstl/functions" prefix = "fn" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>@RequestMapping and @RequestParam test bed</title>
+<style>
+input[type=text] {
+	padding: 10px;
+	margin: 10px;
+	font-size: 20px
+}
+
+input[type=submit] {
+	border: none;
+	background-color: #4CAF50;
+	color: white;
+	padding: 16px;
+	margin: 16px;
+	cursor: pointer;
+	font-size: 20px
+}
+
+h2 {
+	color: #08298A;
+	text-align: center
+}
+</style>
+</head>
+<body>
+	<div align="left">
+		<h2 align="center">Hello, Welcome to the ${fn:escapeXml("@")}RequestMapping & RequestParam Test Bed</h2>
+		<hr />
+		
+		<form action="test1">
+			<h3>test 1: Testing @RequestParam without explicit attributes</h3>
+			<label id="organization-name">Organization Name</label>
+			<input type="text" name="orgname" placeholder="Enter organization name"	size="40" /> 
+			<input type="submit" value="Submit" />
+		</form>
+		
+		<br /> <br>
+		<form action="test2">
+			<h3>test 2: Testing @RequestMapping 'method' attribute</h3>
+			<label id="organization-name">Organization Name</label> 
+			<input type="text" name="orgname" placeholder="Enter organization name"	size="40" />
+			<input type="submit" value="Submit" />
+		</form>
+		
+		<br /> <br>
+		<form action="test3">
+			<h3>test 3: Testing @RequestMapping fallback feature</h3>
+			<input type="submit" value="Submit" />
+		</form>
+		
+		<br /> <br>
+		<form action="test4">
+			<h3>test 4: Testing @RequestParam 'defaultValue' attribute</h3>
+			<label id="organization-name">Organization Name</label>
+			<input type="text" name="orgname" placeholder="Enter organization name"	size="40" />
+			<input type="submit" value="Submit" />
+		</form>
+		
+		<br /> <br>
+		<form action="test5">
+			<h3>test 5: Testing @RequestParam without 'name' or 'value attributes</h3>
+			<label id="organization-name">Organization Name</label>
+			<input type="text" name="orgname" placeholder="Enter organization name"	size="40" />
+			<input type="submit" value="Submit" />
+		</form>
+				
+	</div>
+</body>
+</html>
