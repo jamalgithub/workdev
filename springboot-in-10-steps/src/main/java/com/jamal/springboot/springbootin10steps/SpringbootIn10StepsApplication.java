7c0ef5de469036aa57ae99af2c2d185681f45361
jamalgithub <jelmoubarik@gmail.com>
2017-12-24 16:40:19 +0000

    commit 24/12/2017 16:40

---
+++ springboot-in-10-steps/src/main/java/com/jamal/springboot/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -0,0 +1,17 @@
+package com.jamal.springboot.springbootin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringbootIn10StepsApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+		
+		for (String name : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
+	}
+}
