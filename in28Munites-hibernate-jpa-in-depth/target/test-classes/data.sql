11be1164090353f06fa4eec5ff1b7ce89881cfdb
jamalgithub <jelmoubarik@gmail.com>
2018-02-11 14:39:28 +0000

    commit 11/02/2018 14:40

---
+++ in28Munites-hibernate-jpa-in-depth/target/test-classes/data.sql
@@ -0,0 +1,68 @@
+/*
+create table person
+(
+   id integer not null,
+   name varchar(255) not null,
+   location varchar(255),
+   birth_date timestamp,
+   primary key(id)
+);
+
+*/
+/*
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10001,'Jamal', 'Tetouan', sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10002,'Adil', 'Tanger', sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10003,'Nabil', 'Rabat', sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10004,'Karim', 'Sale', sysdate());
+*/
+
+insert into course(id, name, created_date, last_updated_date,is_deleted) 
+values(10001,'JPA in 50 Steps', sysdate(), sysdate(),false);
+insert into course(id, name, created_date, last_updated_date,is_deleted) 
+values(10002,'Spring in 50 Steps', sysdate(), sysdate(),false);
+insert into course(id, name, created_date, last_updated_date,is_deleted) 
+values(10003,'Spring Boot in 100 Steps', sysdate(), sysdate(),false);
+insert into course(id, name, created_date, last_updated_date, is_deleted)
+values(10004,'Dummy1', sysdate(), sysdate(), false);
+insert into course(id, name, created_date, last_updated_date, is_deleted) 
+values(10005,'Dummy2', sysdate(), sysdate(),false);
+insert into course(id, name, created_date, last_updated_date, is_deleted) 
+values(10006,'Dummy3', sysdate(), sysdate(),false);
+insert into course(id, name, created_date, last_updated_date, is_deleted) 
+values(10007,'Dummy4', sysdate(), sysdate(),false);
+insert into course(id, name, created_date, last_updated_date, is_deleted) 
+values(10008,'Dummy5', sysdate(), sysdate(),false);
+
+insert into passport(id,number)
+values(40001,'E123456');
+insert into passport(id,number)
+values(40002,'N123457');
+insert into passport(id,number)
+values(40003,'L123890');
+
+insert into student(id,name,passport_id)
+values(20001,'Ranga',40001);
+insert into student(id,name,passport_id)
+values(20002,'Adam',40002);
+insert into student(id,name,passport_id)
+values(20003,'Jane',40003);
+
+insert into review(id,rating,description,course_id)
+values(50001,'FIVE', 'Great Course',10001);
+insert into review(id,rating,description,course_id)
+values(50002,'FOUR', 'Wonderful Course',10001);
+insert into review(id,rating,description,course_id)
+values(50003,'FIVE', 'Awesome Course',10003);
+
+insert into student_course(student_id,course_id)
+values(20001,10001);
+insert into student_course(student_id,course_id)
+values(20002,10001);
+insert into student_course(student_id,course_id)
+values(20003,10001);
+insert into student_course(student_id,course_id)
+values(20001,10003);
