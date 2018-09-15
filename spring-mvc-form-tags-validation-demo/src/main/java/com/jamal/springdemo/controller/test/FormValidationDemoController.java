30de2a908807f88468d018c7afebb2f180aabcb4
jamalgithub <jelmoubarik@gmail.com>
2018-09-15 14:12:06 +0100

    14:13 15/09/2018

--- spring-mvc-form-tags-validation-demo/src/main/java/com/jamal/springdemo/controller/test/FormValidationDemoController.java
+++ spring-mvc-form-tags-validation-demo/src/main/java/com/jamal/springdemo/controller/test/FormValidationDemoController.java
@@ -6,11 +6,14 @@ import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -51,4 +54,9 @@ public class FormValidationDemoController {
 		}
 	}
 
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 }
--- spring-mvc-form-tags-validation-xml-demo/src/main/java/com/jamal/springdemo/controller/test/FormValidationDemoController.java
+++ spring-mvc-form-tags-validation-xml-demo/src/main/java/com/jamal/springdemo/controller/test/FormValidationDemoController.java
@@ -6,11 +6,14 @@ import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,34 +24,41 @@ import com.jamal.springdemo.domain.test.OrganizationRepresentative;
 @Controller
 @RequestMapping("/formValidationDemo")
 public class FormValidationDemoController {
-	
+
 	private static Logger LOGGER = LoggerFactory.getLogger(FormValidationDemoController.class);
-	
+
 	@RequestMapping("/home")
 	public ModelAndView home(Model model) {
-		return new ModelAndView("test/formValidationTestViews/formValidationHome", "orgrep", new OrganizationRepresentative());
+		return new ModelAndView("test/formValidationTestViews/formValidationHome", "orgrep",
+				new OrganizationRepresentative());
 	}
-	
-	@RequestMapping(value="/registerOrgRep", method= RequestMethod.POST)
-	public String organizationRepresentativeRegistration(@Valid @ModelAttribute("orgrep") OrganizationRepresentative orgRepresentative,
-					BindingResult result, Model model) {
+
+	@RequestMapping(value = "/registerOrgRep", method = RequestMethod.POST)
+	public String organizationRepresentativeRegistration(
+			@Valid @ModelAttribute("orgrep") OrganizationRepresentative orgRepresentative, BindingResult result,
+			Model model) {
 		// debug code
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			model.addAttribute("formerrors", result.getAllErrors());
 			List<FieldError> ferrList = result.getFieldErrors();
-			for(FieldError ferr: ferrList) {
+			for (FieldError ferr : ferrList) {
 				LOGGER.info("field error: " + ferr.getDefaultMessage());
 			}
-			
+
 			List<ObjectError> gerrFeList = result.getGlobalErrors();
-			for(ObjectError gerr: gerrFeList) {
+			for (ObjectError gerr : gerrFeList) {
 				LOGGER.info("global error: " + gerr.getDefaultMessage());
 			}
-			
+
 			return "test/formValidationTestViews/formValidationHome";
 		} else {
 			return "test/formValidationTestViews/formValidationResult";
 		}
 	}
 
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 }
