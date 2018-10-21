dea9682f164849717ba8fbf2712964eb4adceaaf
jamalgithub <jelmoubarik@gmail.com>
2018-10-21 16:52:50 +0100

    16:45 21/10/2018

---
+++ messenger-master/src/main/java/org/koushik/javabrains/messenger/model/Link.java
@@ -0,0 +1,24 @@
+package org.koushik.javabrains.messenger.model;
+
+public class Link {
+	
+	private String link;
+	private String rel;
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getRel() {
+		return rel;
+	}
+
+	public void setRel(String rel) {
+		this.rel = rel;
+	}
+
+}
